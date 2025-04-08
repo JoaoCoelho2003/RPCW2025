30f74f87dfb0be7a9f23a9b6de317e073a72f5fc
jojocoelho <jhcoelho03@gmail.com>
2025-04-08 18:32:14 +0100

    feat: ponto 11 - queries

---
+++ RPCW2025-Ficha-Medicina/ex2/queries_doentes.py
@@ -0,0 +1,56 @@
+from rdflib import OWL, RDF, Graph, Namespace
+
+n = Namespace("http://www.example.org/disease-ontology#")
+
+g = Graph()
+g.parse("med_doentes.ttl")
+
+print("Quantas doenças estão presentes na ontologia?")
+
+q = """
+SELECT (COUNT(?d) AS ?number)
+WHERE {
+	?d a :Disease .
+}
+"""
+
+for r in g.query(q):
+    print(f"Number of diseases: {r[0]}")
+
+print("\nQue doenças estão associadas ao sintoma yellowish_skin?")
+
+q = """
+SELECT ?d {
+	?d a :Disease .
+	?d :hasSymptom :yellowish_skin .
+}
+"""
+
+for r in g.query(q):
+    print(f"Disease: {r[0].split('#')[1]}")
+
+print("\nQue doenças estão associadas ao tratamento exercise?")
+
+q = """
+SELECT ?d {
+	?d a :Disease .
+	?d :hasTreatment :exercise .
+}
+"""
+
+for r in g.query(q):
+    print(f"Disease: {r[0].split('#')[1]}")
+
+print("\nProduz uma lista ordenada alfabeticamente com o nome dos doentes.")
+
+q = """
+SELECT ?p
+WHERE {
+	?p a :Patient .
+	?p :hasName ?name .
+}
+ORDER BY ?name
+"""
+
+for r in g.query(q):
+    print(f"Patient: {r[0].split('#')[1]}")
